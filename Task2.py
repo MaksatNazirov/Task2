ad68e6cb5d4aa95e46a1ac9c2b61b7984d538d9b
Maks <nazirov.maksat96@gmail.com>
2019-10-23 15:15:41 +0600

    Task2

---
+++ Task2.py
@@ -0,0 +1,11 @@
+ def square_number(n):
+   return n ** 2
+
+
+ def test_square_number():
+   assert square_number(2) == 4
+   assert square_number(8) == 64
+   assert square_number(10) == 100
+   print("YOUR CODE IS CORRECT!")
+ test_square_number()
+
